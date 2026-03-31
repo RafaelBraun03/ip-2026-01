b560437bd6cfff0fdbb78452201ba9b046dfc403
RafaelBraun03 <braunrafael03@gmail.com>
2026-03-30 21:00:43 -0300

    exercícios de sala

---
+++ Sala.go/aula6/exemplo1.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main() {
+  const numNotas int = 3 // Quantidade de notas
+  var (
+	nota [numNotas]float64 // Array de notas
+	soma float64 = 0
+  )
+  // Parte 1 – Leitura
+  for i := 0; i < numNotas; i++ {
+	fmt.Printf("Informe a nota %d: ", i )
+	fmt.Scan(&nota[i]) // Armazena nota no array
+	soma += nota[i] // Calcula a soma das notas
+  }
+  
+  for i, v := range nota {
+  	  fmt.Printf("Nota %d = %f\n", i, v)
+  }
+  fmt.Printf("Soma da notas %f: ", soma)
+}
